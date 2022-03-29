b6a0cf17e70dce32aebf9a3873abe0d9bd5c1fb3
AnnaSobchenko <annsbchnk@gmail.com>
2022-03-29 12:36:09 +0300

    loader added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import ContactList from './Components/ContactList/ContactList.jsx';
 import Filter from './Components/Filter/Filter.jsx';
 import Form from './Components/Form/Form.jsx';
 import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { getContacts } from 'redux/contacts/contactsOperations';
 const { Rings } = require('react-loader-spinner');
 
 const App = () => {
   const dispatch = useDispatch();
+  const isLoading=useSelector(state=>state.contacts.isLoading)
 
   useEffect(() => {
     dispatch(getContacts());
@@ -24,7 +25,7 @@ const App = () => {
         <Form />
         <Filter />
         <ul className="list">
-          {/* isLoading&&<Rings heigth="34" width="100%" color="#fff" ariaLabel='loading' /> */}
+         { isLoading &&<Rings heigth="34" width="100%" color="#fff" ariaLabel='loading' />}
           <ContactList />
         </ul>
       </main>
